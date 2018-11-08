ab1ffdf4bfd597919913800fde26d978a9e94d6b
wminshew <wminshew@gmail.com>
2018-11-08 14:05:21 -0800

    added netlify redirect & not-found component for proper vue router history mode handling

--- src/router.ts
+++ src/router.ts
@@ -57,6 +57,15 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "register" */ "./views/Register.vue")
+    },
+    {
+      path: "*",
+      name: "notFound",
+      // route level code-splitting
+      // this generates a separate chunk (notfound.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(/* webpackChunkName: "notfound" */ "./views/NotFound.vue")
     }
   ]
 });
---
+++ src/views/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <img 
+      alt="emrys logo" 
+      src="@/assets/logo.png"
+    >
+    <div>Page not found</div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "NotFound"
+});
+</script>
