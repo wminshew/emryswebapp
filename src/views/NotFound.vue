3a2167f658e8c83bb65cd3252f2a9aa6d5de70f1
wminshew <wminshew@gmail.com>
2018-12-03 22:04:58 -0800

    updated std 404 page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,10 +1,14 @@
 <template>
-  <div>
-    <img 
-      alt="emrys logo" 
-      src="@/assets/logo.png"
+  <div
+    id="not-found"
+    class="flex text-center justify-center px-4 py-8 mx-auto"
+  >
+    <div
+      class="w-full max-w-lg"
+      style="min-height: 70vh;"
     >
-    <div>Page not found</div>
+      We're sorry, we don't recognize the page you've requested.
+    </div>
   </div>
 </template>
 
