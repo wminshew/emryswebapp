9743a23ec8efb4286452645ce8dd9653ce374b7c
wminshew <wminshew@gmail.com>
2019-07-12 14:30:14 -0700

    revised banner language

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -9,7 +9,7 @@
       <div
         class="max-w-lg flex flex-grow flex-no-wrap px-2 py-0 text-xs text-white text-left"
       >
-        <span>Use promo {{ bannerPromo }} for $50 credit</span>
+        <span>Register with promo {{ bannerPromo }} to receive $50 in emrys credit for your early jobs</span>
       </div>
     </div>
   </router-link>
